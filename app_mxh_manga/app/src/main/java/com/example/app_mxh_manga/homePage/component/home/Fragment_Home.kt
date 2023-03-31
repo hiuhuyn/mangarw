f450194b2cf0d64331decf0f5332294729b2df6a
Nguyễn Minh Quân <83531095+izumi291122@users.noreply.github.com>
2023-03-31 21:57:19 +0700

    sửa lại logo app

--- app_mxh_manga/app/src/main/java/com/example/app_mxh_manga/homePage/component/home/Fragment_Home.kt
+++ app_mxh_manga/app/src/main/java/com/example/app_mxh_manga/homePage/component/home/Fragment_Home.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.app_mxh_manga.offline.CheckNetwork
 
